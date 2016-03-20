83fbfc0984d6e6dec6a34549fc137d1ad7688050
Kirill Fomichev <fanatid@ya.ru>
2016-03-20 17:24:22 +0300

    test: tape instead ava

--- test/recent.js
+++ test/recent.js
@@ -1,14 +1,12 @@
-var test = require('ava')
+var test = require('tape').test
 var shapeshift = require('../')
-
-test = test.cb
 
 test('should get a list of recent transactions', function (t) {
   t.plan(8)
   shapeshift.recent(function (err, recent) {
     t.ifError(err, 'no error')
     t.true(Array.isArray(recent), 'recent is an array')
-    t.is(recent.length, 50, 'recent array should be 50')
+    t.same(recent.length, 50, 'recent array should be 50')
     t.true('curIn' in recent[0], 'has curIn field')
     t.true('curOut' in recent[0], 'has curOut field')
     t.true('timestamp' in recent[0], 'has timestamp field')
